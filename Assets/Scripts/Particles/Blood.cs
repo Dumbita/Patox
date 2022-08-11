82c141b2b5d309d79b34921f906d7d5040e96f0a
PatoxProgramador <RM87630@fiap.com.br>
2022-08-11 17:57:42 -0300

    a

---
+++ Assets/Scripts/Particles/Blood.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem collectParticle = null;
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Collect();
+
+        }
+
+    }
+
+    public void Collect()
+    {
+
+        collectParticle.Play();
+
+    }
+
+}
